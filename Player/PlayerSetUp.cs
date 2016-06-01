16428cb5d93b91463ca50c3bde159c6ec1f38885
Mahdad.Baghani <mahdad.baghani@outlook.com>
2016-06-01 04:56:04 +0430

    # added Player/PlayerSetUp: controlling the spawn process of the local and on-server players

---
+++ Player/PlayerSetUp.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+
+public class PlayerSetUp : NetworkBehaviour 
+{
+    [SerializeField]
+    Behaviour[] _OnNetDisabledComps;
+    Camera sceneCamera;
+
+    private void InitializePlayer()
+    {
+        // Differentiates between local and online players
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < _OnNetDisabledComps.Length; i++)
+            {
+                _OnNetDisabledComps[i].enabled = false;                
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
